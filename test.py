525df8d6a6ac0d8ecc055d51af24eeca985c0dd5
Caleb Carithers <calebcarithers@me.com>
2021-01-10 15:21:06 -0600

    test

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/update_data", methods=["POST"])
 def hello():
+	"""
+	Client sends position clicked
+	Generate random string
+	Send google api requests to get image
+	Store position, string, image url in DB
+	Return to all connected clients via WS
+	"""
 	return "hello"
 
+@app.route("/bitch")
+def haha():
+	return "oh nice"
 
 
