3c0ac2da53b984c815ea1f7fc6f8a191f7b297fb
Imran Adamjee <imey2597@gmail.com>
2021-03-06 09:05:53 -0600

    random pics/sizes

--- client/src/makeImage.js
+++ client/src/makeImage.js
@@ -1,8 +1,9 @@
 const makeImage = (context, x, y, image) => {
     const img = new Image();
     img.src = image;
+    const size = Math.floor(Math.random() * 500) + 100;
     img.onload = () => {
-        context.drawImage(img, x, y, 100, 100)
+        context.drawImage(img, x, y, size, size)
     }
 }
 
